e21f4f2bf4fd923a0fb735bdf8b0c380f47a6a6c
Andy Green <hello@andygrn.co.uk>
2015-03-17 10:38:37 +0000

    Added close button to cookie notice - closes #21

--- raw/js/load.js
+++ raw/js/load.js
@@ -4,6 +4,9 @@
 	if( document.cookie.indexOf( 'seenCookieNotice' ) === -1 ){
 		classie.addClass( document.body, 'show-cookie-notice' );
 		document.cookie = 'seenCookieNotice=yes;path=/;max-age=31536000';
+		eventie.bind( document.querySelector( '.cookie-notice button' ), 'click', function(){
+			classie.removeClass( document.body, 'show-cookie-notice' );
+		} );
 	}
 
 } )();
