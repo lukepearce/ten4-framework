54843af108f4aa9a88bda8af8aa15c5b8e122550
Andy Green <hello@andygrn.co.uk>
2015-05-12 11:19:45 +0100

    Give cookie-notice js its own scope

--- raw/js/load.js
+++ raw/js/load.js
@@ -1,12 +1,15 @@
 ( function(){
 	'use strict';
 
-	if( document.cookie.indexOf( 'seenCookieNotice' ) === -1 ){
-		classie.addClass( document.body, 'show-cookie-notice' );
-		document.cookie = 'seenCookieNotice=yes;path=/;max-age=31536000';
-		eventie.bind( document.querySelector( '.cookie-notice button' ), 'click', function(){
-			classie.removeClass( document.body, 'show-cookie-notice' );
-		} );
-	}
+	( function(){
+		// Cookie notice
+		if( document.cookie.indexOf( 'seenCookieNotice' ) === -1 ){
+			classie.addClass( document.body, 'show-cookie-notice' );
+			document.cookie = 'seenCookieNotice=yes;path=/;max-age=31536000';
+			eventie.bind( document.querySelector( '.cookie-notice button' ), 'click', function(){
+				classie.removeClass( document.body, 'show-cookie-notice' );
+			} );
+		}
+	} )();
 
 } )();
